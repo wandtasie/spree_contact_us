a0a6066b8e69b8260299a06b55d0b22b6110073b
Matthias Wagner <matthias.wagner@online.de>
2012-08-14 16:27:26 +0200

    kontakt link changged

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Spree::Core::Engine.routes.draw do
   resources :contacts,
     :controller => 'contact_us/contacts',
     :only       => [:new, :create]
-  match 'kontaktformular' => 'contact_us/contacts#new', :as => :contact_us
+  match 'kontakt' => 'contact_us/contacts#new', :as => :contact_us
 end
